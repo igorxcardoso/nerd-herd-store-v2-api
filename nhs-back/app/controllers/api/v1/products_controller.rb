3cf9df3d7a4900f213626d0eb7cc973a710a663f
Igor Cardoso <iygorsilva@gmail.com>
2021-12-29 02:39:23 -0300

    create routes for products

---
+++ nhs-back/app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::ProductsController < ApplicationController
+
+  # GET api/v1/products
+  def index
+    @products = Product.all
+
+    render json: @products
+  end
+
+  # GET api/v1/products/1
+  def show
+    @product = Product.find(params[:id])
+
+    if @product.nil?
+      render json: ErrorSerializer.serialize(@product.errors), status: :unprocessable_entity
+    else
+      render json: @product
+    end
+  end
+
+  # POST api/v1/products
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT api/v1/products/1
+  def update
+  end
+
+  # DELETE api/v1/products/1
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def product_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+  end
+end
--- nhs-back/app/serializers/product_serializer.rb
+++ nhs-back/app/serializers/product_serializer.rb
@@ -1,3 +1,3 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :title, :code_bar, :created_at, :updated_at
 end
