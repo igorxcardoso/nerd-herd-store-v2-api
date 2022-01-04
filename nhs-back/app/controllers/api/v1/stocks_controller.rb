cd7f579145c132ea648147d0cd64220ba6f65759
Igor Cardoso <iygorsilva@gmail.com>
2022-01-04 16:44:02 -0300

    add relationship, and creation update action using before_action

--- nhs-back/app/controllers/api/v1/stocks_controller.rb
+++ nhs-back/app/controllers/api/v1/stocks_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::StocksController < ApplicationController
-    # GET api/v1/stocks
+  before_action :set_stock, only: %i[show update destroy]
+
+  # GET api/v1/stocks
   def index
     @stocks = Stock.all
 
@@ -8,8 +10,6 @@ class Api::V1::StocksController < ApplicationController
 
   # GET api/v1/stocks/1
   def show
-    @stock = Stock.find(params[:id])
-
     if @stock.nil?
       render json: ErrorSerializer.serialize(@stock.errors), status: :unprocessable_entity
     else
@@ -30,6 +30,11 @@ class Api::V1::StocksController < ApplicationController
 
   # PATCH/PUT api/v1/stocks/1
   def update
+    if @stock.update(stock_params)
+      render json: @stock, status: :created
+    else
+      render json: @stock.erros, status: :unprocessable_entity
+    end
   end
 
   # DELETE api/v1/stocks/1
@@ -38,6 +43,10 @@ class Api::V1::StocksController < ApplicationController
   end
 
   private
+
+  def set_stock
+    @stock = Stock.find(params[:id])
+  end
 
   def stock_params
     ActiveModelSerializers::Deserialization.jsonapi_parse(params)
--- nhs-back/app/models/stock.rb
+++ nhs-back/app/models/stock.rb
@@ -1,3 +1,3 @@
 class Stock < ApplicationRecord
-  belongs_to :product
+  belongs_to :product   # relationship
 end
