9e884dbc8a3bd4d2fc28b8695a61f862966d20e8
Igor Cardoso <iygorsilva@gmail.com>
2022-01-04 16:42:47 -0300

    add validade and relationship

--- nhs-back/app/models/product.rb
+++ nhs-back/app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   has_many :contents, dependent: :destroy
+  has_many :stocks, dependent: :destroy
 
+  validates :title, presence: true      # title is mandatory
 end
