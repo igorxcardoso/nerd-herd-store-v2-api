1b55cb2731b6e07da0f04291fc3f5d704f6ac8f3
Igor Cardoso <iygorsilva@gmail.com>
2022-01-07 18:24:13 -0300

    seeds update

--- nhs-back/db/seeds.rb
+++ nhs-back/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+start = DateTime.now
+p "Starting rake: #{start}"
+
+
+# Products and Stocks
+
+10.times do |i|
+    Product.create(title: Faker::Device.model_name,
+                    code_bar: Faker::Barcode.ean,
+                    description: Faker::TvShows::SouthPark.quote,
+                    price_unit: Faker::Number.decimal(l_digits: 3, r_digits: 3)
+    )
+end
