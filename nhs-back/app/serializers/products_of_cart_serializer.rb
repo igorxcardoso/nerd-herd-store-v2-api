499b1697942c34dc1247add405ed9eedfde78004
Igor Cardoso <iygorsilva@gmail.com>
2022-01-28 23:45:50 -0300

    serializer settings

--- nhs-back/app/serializers/products_of_cart_serializer.rb
+++ nhs-back/app/serializers/products_of_cart_serializer.rb
@@ -1,4 +1,8 @@
-class ProductsOfCartSerializer < ActiveModel::Serializer
+class ProductsOfCartSerializer
+  include FastJsonapi::ObjectSerializer
+  set_key_transform :dash
+
+
   attributes :id, :product_id, :length_product, :shopping_cart_id, :price_for_product,
               :off_for_product, :created_at, :updated_at
 end
